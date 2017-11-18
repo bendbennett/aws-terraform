4778c641b2a91cddf81eed630a059253d4474fff
Benjamin Bennett <ben.bennett@everlution.com>
2017-11-18 12:45:15 +0000

    Adding private.tf to hold hosted zone id and name, key name and ssl certificate id.

---
+++ global/private.tf
@@ -0,0 +1,16 @@
+variable hosted_zone_public_id {
+  default = ""
+}
+
+variable hosted_zone_public_name {
+  default = ""
+}
+
+variable key_name {
+  default = ""
+}
+
+variable "ssl_certificate_id" {
+  default = ""
+}
+
