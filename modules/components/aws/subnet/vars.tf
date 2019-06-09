18b853276829522923fb912f408c4d3c056119cf
Benjamin Bennett <synaptology@gmail.com>
2019-06-09 15:21:19 +0100

    Fixing formatting of vars for prod/vpc

--- modules/components/aws/subnet/vars.tf
+++ modules/components/aws/subnet/vars.tf
@@ -1,10 +1,19 @@
-variable "availability_zones" { type = "list" }
+variable "availability_zones" {
+    type = "list"
+}
 
-variable "cidr_blocks" { type = "list" }
+variable "cidr_blocks" {
+    type = "list"
+}
 
-variable "internet_gateway_id" { default = "" }
+variable "internet_gateway_id" {
+    default = ""
+}
 
-variable "nat_gateway_ids" { type = "list" default = [] }
+variable "nat_gateway_ids" {
+    type = "list"
+    default = []
+}
 
 variable "public_subnet" {}
 
--- modules/infrastructure/vpc/vars.tf
+++ modules/infrastructure/vpc/vars.tf
@@ -1,12 +1,23 @@
 variable "cidr_block_vpc" {}
 
-variable "availability_zones" { type = "list" default = [] }
+variable "availability_zones" {
+    type = "list"
+    default = []
+}
 
-variable "cidr_blocks_public" { type = "list" default = [] }
+variable "cidr_blocks_public" {
+    type = "list"
+    default = []
+}
 
-variable "cidr_blocks_private" { type = "list" default = [] }
+variable "cidr_blocks_private" {
+    type = "list"
+    default = []
+}
 
-variable "hosted_zone_private" { type = "map" }
+variable "hosted_zone_private" {
+    type = "map"
+}
 
 variable "hosted_zone_private_prefix" {}
 
